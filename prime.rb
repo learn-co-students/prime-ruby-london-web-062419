c0b149f5aacd82ffd200faa78fc50de0102e04a3
github username <github email address>
2019-06-10 15:38:24 +0100

    prime numbers

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+# # Add  code here!
+#prime numbers are only divisible by themselves or 1
+  def prime?(num)
+    if num < 0 || num == 0 || num == 1 # false statment 
+        return false
+      else
+        (2..num-1).to_a.all? do |int| # fresh up on to_a.all
+          num % int != 0
+        end
+      end
+    end
+
